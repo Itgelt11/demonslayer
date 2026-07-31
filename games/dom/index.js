d4d53cc209cb07e5e3531e5c512ce26b55dde865
26LP95511515 <26LP95511515@administrators-iMac-2B-403.local>
2026-07-31 12:04:45 +0800

    changer

---
+++ games/dom/index.js
@@ -0,0 +1,11 @@
+const noob = document.getElementById("player");
+const button = document.getElementById("button");
+const div = document.getElementById("div");
+button.addEventListener("click", function () {
+  console.log("hello");
+  noob.textContent = "сайн уу";
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
